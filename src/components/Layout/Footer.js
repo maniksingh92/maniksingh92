ac8929adb559f2a51b01dfa50e1ed14a814dd744
Manik Singh <maniksingh92@gmail.com>
2020-03-20 18:20:56 +0530

    Init Layout components

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer className="container">
+      <hr />
+      <div className="text-center py-4">&copy; 2020</div>
+    </footer>
+  )
+}
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 left-0 right-0 py-4 font-type-header bg-dark text-light opacity-95">
+      <nav className="container flex">
+        <Link to="/" className="mr-auto">
+          Manik Singh
+        </Link>
+        <ul className="flex">
+          <li>
+            <Link to="/blog/" className="mx-4">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact/" className="mx-4">
+              Get In Touch
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
