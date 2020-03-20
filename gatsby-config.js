51499572da71ec84b81cc2420353f1e3bd91894d
Manik Singh <maniksingh92@gmail.com>
2020-03-20 17:45:34 +0530

    Configure gatsby scss and purge css plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Manik Singh`,
+    description: `I write code.`,
+    siteUrl: `https://www.manik.codes`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [require("tailwindcss")("./tailwind.config.js")],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        tailwind: true, // Enable tailwindcss support
+      },
+    },
+  ],
 }
