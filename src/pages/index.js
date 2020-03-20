94363c5643515f77b689eec0688d3fb95d6005c4
Manik Singh <maniksingh92@gmail.com>
2020-03-20 18:21:21 +0530

    Create first iteration of Hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import Layout from "../components/Layout"
+
+export default () => (
+  <Layout>
+    <section className="flex container flex-col py-32 justify-center items-center text-center">
+      <div className="mb-16 text-xl">
+        A good software is a product of deep understanding and efficient
+        delivery.
+      </div>
+      <h1 className="flex flex-col text-6xl">
+        <span className="font-bold">Hello.</span>
+        <span>I'm Manik Singh.</span>
+      </h1>
+      <div className="flex items-center">
+        <hr className="w-8" />
+        <span className="mx-4">Programmer</span>
+        <hr className="w-8" />
+      </div>
+    </section>
+  </Layout>
+)
