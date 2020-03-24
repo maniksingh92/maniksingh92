5d818715090dd45997554f0d5ba62a9a5e84d09d
Manik Singh <maniksingh92@gmail.com>
2020-03-25 02:45:58 +0530

    Add sloped bg to hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,32 @@ import Layout from "../components/Layout"
 
 export default () => (
   <Layout>
-    <section className="flex container flex-col py-32 justify-center items-center text-center">
-      <div className="mb-16 text-xl italic">
-        A great software is a product of profound understanding and a simple
-        design.
+    <section
+      id="section-hero"
+      className="pt-48 pb-16 mb-32 text-center bg-pattern bg-slope-bottom"
+    >
+      <div className="container">
+        <div className="mb-16 text-xl italic">
+          A great software is a product of profound understanding and a simple
+          design.
+        </div>
+        <h1 className="flex flex-col text-6xl">
+          <span className="font-bold text-brand">Hello.</span>
+          <span>I'm Manik Singh.</span>
+        </h1>
+        <div className="flex items-center justify-center">
+          <hr className="w-8" />
+          <span className="mx-4">Programmer</span>
+          <hr className="w-8" />
+        </div>
       </div>
-      <h1 className="flex flex-col text-6xl">
-        <span className="font-bold">Hello.</span>
-        <span>I'm Manik Singh.</span>
-      </h1>
-      <div className="flex items-center">
-        <hr className="w-8" />
-        <span className="mx-4">Programmer</span>
-        <hr className="w-8" />
+    </section>
+
+    <section id="section-overview" className="container py-16">
+      <div className="flex justify-center">
+        <h2 className="text-3xl text-center border-t-4 border-b-4 mb-8">
+          Overview
+        </h2>
       </div>
     </section>
   </Layout>
