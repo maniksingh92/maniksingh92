a1905ce39c49f42f4a2bb7cfa14bf704930578d7
Travis CI User <travis@example.org>
2020-03-24 19:56:31 +0000

    Updates

---
+++ component---src-pages-404-js-27388904b08bad096284.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return r}));var a=n("q1tI"),o=n.n(a),c=n("Zttt");function r(){return o.a.createElement(c.a,null,o.a.createElement("section",{className:"container py-32"},o.a.createElement("h1",{className:"text-2xl text-center mb-8"},"The content you were looking for was not found..")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-27388904b08bad096284.js.map
